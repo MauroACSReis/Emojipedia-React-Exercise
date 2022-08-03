8b5103047cad81fdb0e9a0700b05f314d4eb6adb
MauroACSReis <mauroacsreis@gmail.com>
2022-08-02 21:01:59 -0300

    Fix Repo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,15 @@
 import React from "react";
-import Card from "./Card";
-import contacts from "../contacts";
-import Avatar from "./Avatar";
+import emojipedia from "../emojipedia";
+import Entry from "./Entry";
 
-function createCard(contact) {
+function createEntry(emojiTerm) {
   return (
-    <Card
-      id={contact.id}
-      key={contact.id}
-      name={contact.name}
-      img={contact.imgURL}
-      phone={contact.phone}
-      email={contact.email}
+    <Entry
+      id={emojiTerm.id}
+      key={emojiTerm.id}
+      name={emojiTerm.name}
+      emoji={emojiTerm.emoji}
+      description={emojiTerm.meaning}
     />
   );
 }
@@ -19,30 +17,10 @@ function createCard(contact) {
 function App() {
   return (
     <div>
-      <h1 className="heading">My Contacts</h1>
-      <Avatar img="https://media-exp1.licdn.com/dms/image/C4E03AQHdFvilq2eA1w/profile-displayphoto-shrink_800_800/0/1649795706579?e=1664409600&v=beta&t=zoewkk9Vm_yrihdLZjDCLU885RYHrvvcIPe2Zm-lrJ8" />
-      {/* "Mapping" Technique used for contacts.js object array */}
-      {contacts.map(createCard)}
-      {/* Instead of */}
-      {/* <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      />
-      */}
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
     </div>
   );
 }
